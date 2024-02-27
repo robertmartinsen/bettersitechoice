9651d4cdf6fc3511bd5085ed018f573b9527b123
robertmartinsen <robertmartinsen2000@gmail.com>
2024-02-27 20:25:11 +0100

    readme

--- js/home/mail.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Envelope Icon Click Event Handler
-    $('#envelope-icon').click(function() {
-      // Replace 'support@bestsitechoice.com' with your email
-      window.location.href = 'mailto:support@bestsitechoice.com';
-    });
-  
-  });
--- js/home/trucante.js
+++
@@ -1,5 +0,0 @@
-// Truncate paragraph text to a certain number of words
-var paragraph = document.querySelector('.truncate-paragraph');
-var words = paragraph.textContent.split(' ');
-var truncatedText = words.slice(0, 10).join(' ') + '...';
-paragraph.textContent = truncatedText;
